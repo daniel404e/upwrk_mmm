aa0cf85088455e989e0118b8c732bee0429ca966
Daniel Joshua <joshuadaniel404@gmail.com>
2023-04-19 15:09:13 +0530

    Revert "Revert "Milestone 3 - Refactor code""

---
+++ src/components/BaseFilters/index.jsx
@@ -0,0 +1,3 @@
+export { default as ActiveFilters } from "./ActiveFilters";
+export { default as CheckboxFilter } from "./CheckboxFilter";
+export { default as SelectionFilter } from "./SelectionFilter";
